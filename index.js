5bf8d66f58f77b30855932966c660eb63a3d09f4
Amanowsky <adamtroszczynski@outlook.com>
2023-09-14 03:06:49 +0200

    AIR quality has finised, createAirQuality function added

--- index.js
+++ index.js
@@ -1,21 +1,30 @@
 const WEATER_API_KEY = "366e7a28647c4da4803184927231209";
 const BASE_URL = "http://api.weatherapi.com/v1";
 const FORECAST_ENDPOINT = "/forecast.json";
-const MAIN_CURRENT_WEATHER = document.querySelector("#MAIN_CURRENT_WEATHER");
+const FORECAST_KEYWORDS = "&days=7&aqi=yes&lang=pl&alerts=yes";
+const DAYS_TO_FETCH = 7;
 const HOURS_TO_DISPLAY = 13;
+
+const MAIN_CURRENT_WEATHER = document.querySelector("#MAIN_CURRENT_WEATHER");
 const HEADER_CITY = document.querySelector('#HEADER_CITY');
 const HEADER_TEMP = document.querySelector('#HEADER_TEMP');
 const HEADER_WEATHER = document.querySelector('#HEADER_WEATHER');
 const HEADER_HL = document.querySelector('#HEADER_HL');
+const AIR_VALUE = document.querySelector("#AIR_VALUE");
+const AIR_DESC = document.querySelector("#AIR_DESC");
+const AIR_DOT = document.querySelector('#AIR_DOT');
 
 
-const setCurrentTemp = async (weather) => {
+
+
+const setCurrentTemp = (weather) => {
     const {current: {condition: {text},temp_c},location: {name},forecast:{forecastday:[today]}} = weather;
     const {day: {maxtemp_c,mintemp_c}} = today;
     HEADER_CITY.textContent = `${name}`;
     HEADER_TEMP.textContent = `${temp_c}`;
     HEADER_WEATHER.textContent = `${text}`;
     HEADER_HL.textContent = `H:${maxtemp_c}° L:${mintemp_c}°`
+
 }
 
 
@@ -37,9 +46,9 @@ const createCurrentWeather = (temp, time, type, icon) => {
     return div;
 }
 
-const fetchWeatherData = async (city) => {
+const fetchWeatherData = async (city,type,keywords) => {
     try {
-        const response = await fetch(`${BASE_URL}${FORECAST_ENDPOINT}?key=${WEATER_API_KEY}&q=${city}&days=7&aqi=yes&lang=pl&alerts=yes`);
+        const response = await fetch(`${BASE_URL}${type}?key=${WEATER_API_KEY}&q=${city}${keywords}`);
         if (!response.ok) {
             throw new Error(`${response.status}`);
         }
@@ -50,7 +59,7 @@ const fetchWeatherData = async (city) => {
     }
 }
 
-const forecastGenerate = async (weather) => {
+const createForecastWeather = async (weather) => {
     const hour = new Date().getHours();
     let nextDay = 0;
     let isFirst = true;
@@ -74,14 +83,26 @@ const forecastGenerate = async (weather) => {
     weatherDivs.forEach((div) => {
         MAIN_CURRENT_WEATHER.appendChild(div);
     })
+
+}
+
+const createAirQuality = (weather,history) => {
+    const {current : {air_quality: {co,o3}}} = weather;
+    const grade = co < 350 ? "Bardzo dobra":
+                 co < 700 ? "Dobra": 
+                 co < 1000 ? "Zła": 
+                 co < 1400 ? "Bardzo zła": "Niebezpieczna";
+    AIR_VALUE.innerText = `${co} - ${grade}`;
+    AIR_DESC.innerText = `Ilość o3 w powietrzu wynosi ${o3}.`;
+    document.documentElement.style.setProperty('--airqualityDot',`${co/20}%`);
 }
 
 const mainApp = async(city) => {
-    const weather = await fetchWeatherData(city);
-    await setCurrentTemp(weather);
+    const weather = await fetchWeatherData(city,FORECAST_ENDPOINT,FORECAST_KEYWORDS);
     console.log(weather);
-    await forecastGenerate(weather);
-
+    setCurrentTemp(weather);
+    createForecastWeather(weather);
+    createAirQuality(weather);
 }
 
 mainApp('walbrzych');
